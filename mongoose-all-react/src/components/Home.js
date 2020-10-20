903720f6fd669c376c5d583eb85dca40ff57c2aa
Chanuka <chanursandeepa@gmail.com>
2020-10-20 14:24:53 +0530

    home component

--- mongoose-all-react/src/components/Home.js
+++ mongoose-all-react/src/components/Home.js
@@ -1,11 +1,76 @@
 import React from 'react'
 import '../css/home.css'
-import Header from './Header'
+import Carousel from 'infinite-react-carousel';
 
 export default function Home() {
+
+    const settings = {
+        arrows: false,
+        autoplay: true,
+        duration: 100
+    };
+
     return (
-        <div className="home">
-            <Header />
+        <div className="left-content">
+            <div className="carousel">
+                <Carousel style={{ height: '80%' }} {...settings}>
+                    <div className="caro">
+                        <img height="100%" src={require("../assests/image-1.png")} />
+                        {/* <p className="legend">Legend 1</p> */}
+                    </div>
+                    <div className="caro">
+                        <img height="100%" src={require("../assests/image-2.jpg")} />
+                        {/* <p className="legend">Legend 2</p> */}
+                    </div>
+                    <div className="caro">
+                        <img height="100%" src={require("../assests/image-3.jpg")} />
+                        {/* <p className="legend">Legend 3</p> */}
+                    </div>
+                </Carousel>
+            </div>
+            <div className="home-category-container">
+                <div className="category-field">
+
+                </div>
+                <div className="category-field">
+
+                </div>
+                <div className="category-field">
+
+                </div>
+                <div className="category-field">
+
+                </div>
+            </div>
+            <div className="discount-container">
+                <div className="sales-container">
+                    <div className="sales-image">
+                        <img src={require('../assests/big-sale.svg')} />
+                    </div>
+                    <div className="sales-details">
+                        <div className="sales-day-count">
+                            <h3>Sales End In</h3>
+                            <div className="timer">
+                                <div> 01 </div>
+                                <h4>days</h4>
+                                <div>19</div>
+                                <h4>:</h4>
+                                <div>19</div>
+                                <h4>:</h4>
+                                <div>19</div>
+                            </div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="offer-container">
+
+                </div>
+            </div>
         </div>
+
+
     )
 }
--- mongoose-all-react/src/components/Left.js
+++ mongoose-all-react/src/components/Left.js
@@ -1,42 +1,15 @@
 import React from 'react'
 import '../css/left.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import Carousel from 'infinite-react-carousel';
+
 import Header from './Header';
+import Home from './Home';
 
 export default function Left() {
-
-    const settings = {
-        arrows: false,
-        autoplay: true,
-        duration: 100
-    };
-
     return (
         <div className="left">
             <Header />
-            <div className="left-content">
-                <div className="carousel">
-                    <Carousel {...settings}>
-                        <div>
-                            <img src={require("../assests/image-1.png")} />
-                            {/* <p className="legend">Legend 1</p> */}
-                        </div>
-                        <div>
-                            <img src={require("../assests/image-2.jpg")} />
-                            {/* <p className="legend">Legend 2</p> */}
-                        </div>
-                        <div>
-                            <img src={require("../assests/image-3.jpg")} />
-                            {/* <p className="legend">Legend 3</p> */}
-                        </div>
-                    </Carousel>
-                </div>
-            </div>
-
-            <div className="">
-
-            </div>
+            <Home />
         </div>
     )
 }
